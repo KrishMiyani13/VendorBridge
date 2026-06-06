170abcf0f1cc33680bae7fe9f2b8ff19160645c0
DhruvGhevariya <ghevariyadhruv306@gmail.com>
2026-06-06 11:36:07 +0530

    Refactor settings and urls; update models and views for accounts app

--- VendorBridge/urls.py
+++ VendorBridge/urls.py
@@ -15,20 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
+
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+      path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
+   
     path('vendors/', include('vendors.urls')),
     path('rfq/', include('rfq.urls')),
-=======
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
->>>>>>> 83c76396f548d737eed7f6a5ad1284475aed2f8c
-]
+    path('quotations/', include('quotations.urls')),
+    path('approvals/', include('approvals.urls')),
+    path('procurement/', include('procurement.urls')),
+    path('analytics/', include('analytics_app.urls')),
+] 
